5ff756b44dd181762b88843c2cffb48ed31c3e59
jonsmacbookair <jonathan.morrisseyto@gmail.com>
2024-07-25 11:11:55 +0100

    added win and draw functionality

--- src/Main.java
+++ src/Main.java
@@ -10,10 +10,17 @@ public class Main {
 
         Scanner scanner = new Scanner(System.in);
         int input;
+        char turn = 'X';
+        int currentTurn = 1;
+
+
         while (true) {
-            System.out.print("Enter a position: ");
-            input = scanner.nextInt();
-            pos[input - 1] = 'X';
+            do {
+                System.out.print("Enter a position: ");
+                input = scanner.nextInt();
+            } while (pos[input -1] == 'X' || pos[input -1] == 'O');
+
+            pos[input - 1] = turn;
 
             System.out.println(" " + pos[6] + " | " + pos[7] + " | " + pos[8] + " ");
             System.out.println("---+---+---");
@@ -21,6 +28,34 @@ public class Main {
             System.out.println("---+---+---");
             System.out.println(" " + pos[0] + " | " + pos[1] + " | " + pos[2] + " ");
 
+            if (pos[0] == turn && pos[1] == turn && pos[2] == turn
+                    || pos[3] == turn && pos[4] == turn && pos[5] == turn
+                    || pos[6] == turn && pos[7] == turn && pos[8] == turn
+                    || pos[6] == turn && pos[3] == turn && pos[0] == turn
+                    || pos[7] == turn && pos[4] == turn && pos[1] == turn
+                    || pos[8] == turn && pos[5] == turn && pos[2] == turn
+                    || pos[6] == turn && pos[4] == turn && pos[2] == turn
+                    || pos[8] == turn && pos[4] == turn && pos[0] == turn) {
+                System.out.println(turn + " is the WINNER!");
+                break;
+            }
+                     {
+
+
+            }
+
+            if (turn == 'X') {
+                turn = 'O';
+            }
+            else if (turn == 'O') {
+                turn = 'X';
+            }
+
+            currentTurn++;
+            if(currentTurn > 9){
+                System.out.println("DRAW");
+                break;
+            }
 
         }
     }
