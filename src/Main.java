25dc08172a3a0687ef16570629815ac6ab41aa7a
Jonathan Morrissey <jonsmacbookair@jonathanslaptop.lan>
2024-07-25 09:48:23 +0100

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,22 @@
+// Jons Version of console Tic Tac Toe
+
+
+public class Main {
+    public static void main(String[] args) {
+
+        System.out.println("   |   |   ");
+        System.out.println("-----------");
+        System.out.println("   |   |   ");
+        System.out.println("-----------");
+        System.out.println("   |   |   ");
+        System.out.println("-----------");
+
+
+    }
+}
+
+//   |   |
+//----------
+//   |   |
+//----------
+//   |   |
